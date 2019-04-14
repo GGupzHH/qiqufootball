7141813e1f989f3d18e6f6400d902eea5502476f
zhaoyagu <zhao.yaguo@shinewingdigital.com>
2019-04-14 11:55:48 +0800

    index页面创建

--- src/qiquIndex.vue
+++ src/qiquIndex.vue
@@ -5,7 +5,7 @@
       <router-link to="/CompetitionArea">赛区</router-link> |
       <router-link to="/CompetitionScoreList">赛事积分榜</router-link> |
       <router-link to="/TeamInformation">球队信息</router-link> |
-      <router-link to="/EventReview">赛事回顾</router-link> |
+      <router-link to="/EventReview">赛事回顾</router-link>
       <!-- <router-link to="/about">About</router-link> -->
     </div>
     <router-view/>
--- src/router.js
+++ src/router.js
@@ -8,6 +8,10 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
+      path: '/',
+      name: 'index',
+      component: () => import('./views/index.vue')
+    }, {
       // 赛区
       path: '/CompetitionArea',
       name: 'CompetitionArea',
@@ -93,8 +97,8 @@ export default new Router({
       children: [
         {
           // 精彩回顾
-          path: '/WonderfulReview',
-          name: '/WonderfulReview',
+          path: 'WonderfulReview',
+          name: 'WonderfulReview',
           component: () => import('./components/WonderfulReview.vue')
         }
       ]
